6be37287a98ff386222e719b78f1a4724293ae99
tananugrah <tananugrahr@gmail.com>
2022-08-18 10:55:28 +0700

    second commit

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
 
+import os
+import json
+import psycopg2
+
 if __name__ == '__main__':
-    print("hello")
+    path = os.getcwd()
+    with open(path+'/'+'config.json') as file:
+        conf = json.load(file)['postgresql']
+    try:
+        conn = psycopg2.connect(host=conf['host'],
+                            database=conf['db'],
+                            user=conf['user'],
+                            password=conf['pwd'])
+        print(f"[INFO] success connect postgresql ..")
+    except:
+        print(f"[INFO] can't connect postgresql ..")
